ebf3ec138275ec169599bb5deba40ed41941d28e
kinbou <c0b221431e@edu.teu.ac.jp>
2024-11-19 16:13:40 +0900

    練習7：ループの解消

--- pygame_image.py
+++ pygame_image.py
@@ -10,14 +10,18 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #背景画像Surfaceを作成
+    bg_img2 = pg.image.load("fig/pg_bg.jpg") #背景画像7-1
     kk_img= pg.image.load("fig/3.png") #こうかとん画像Surfaceを作成
     kk_img= pg.transform.flip(kk_img, True, False) #こうかとん画像の反転
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr%800 
-        screen.blit(bg_img, [-x, 0]) #screen Surfaceに背景画像を張り付ける
+        x = -(tmr%3200) 
+        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img2, [x+1600, 0])
+        screen.blit(bg_img, [x+3200, 0])
+        screen.blit(bg_img2, [x+4800, 0])
         screen.blit(kk_img, [300, 200]) #screen Surfaceにこうかとん画像を張り付ける
         pg.display.update()
         tmr += 1        
