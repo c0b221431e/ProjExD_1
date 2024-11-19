6701c556057ea81476ab0512d9b80a60b0b5c6f3
kinbou <c0b221431e@edu.teu.ac.jp>
2024-11-19 16:36:46 +0900

    練習８：こうかとん移動

--- pygame_image.py
+++ pygame_image.py
@@ -11,18 +11,31 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #背景画像Surfaceを作成
     bg_img2 = pg.image.load("fig/pg_bg.jpg") #背景画像7-1
-    kk_img= pg.image.load("fig/3.png") #こうかとん画像Surfaceを作成
-    kk_img= pg.transform.flip(kk_img, True, False) #こうかとん画像の反転
+    kk_img = pg.image.load("fig/3.png") #こうかとん画像Surfaceを作成
+    kk_img = pg.transform.flip(kk_img, True, False) #こうかとん画像の反転
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+            
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0, +1)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(+1, 0)
+        
         x = -(tmr%3200) 
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img2, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(bg_img2, [x+4800, 0])
-        screen.blit(kk_img, [300, 200]) #screen Surfaceにこうかとん画像を張り付ける
+        screen.blit(kk_img, kk_rct) #screen Surfaceにこうかとん画像を張り付ける
         pg.display.update()
         tmr += 1        
         clock.tick(200)
